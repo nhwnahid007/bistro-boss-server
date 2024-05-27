8785628eeadbd7b60f6133cd599ac038b5d16036
nhwnahid007 <nhwnahid@gmail.com>
2024-05-27 19:46:48 +0600

    added delete operation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
@@ -36,14 +36,21 @@ async function run() {
       res.send(result);
     });
 
-    //carts collection
+  
+   //carts collection
+  app.get('/carts', async (req, res) => {
+    const email = req.query.email;
+    const query = { email: email };
+    const result = await cartsCollection.find(query).toArray();
+    res.send(result);
+  });
 
-    app.get('/carts',async(req,res)=>{
-      const email = req.query.email
-      const query = {email: email}
-      const result = await cartsCollection.find(query).toArray()
-      res.send(result)
-    })
+
+  // app.get('/carts', async (req, res) => {
+ 
+  //   const result = await cartsCollection.find().toArray();
+  //   res.send(result);
+  // });
 
 
     app.post("/carts", async (req, res) => {
@@ -51,6 +58,13 @@ async function run() {
       const result = await cartsCollection.insertOne(cartItems);
       res.send(result);
     });
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
