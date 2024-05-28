eeff0aa7f53f3c11dc8e71da39896571da886897
nhwnahid007 <nhwnahid@gmail.com>
2024-05-28 10:16:44 +0600

    if user is exist return null

--- index.js
+++ index.js
@@ -33,6 +33,14 @@ async function run() {
 
     app.post('/users', async(req,res)=>{
       const user = req.body
+      // insert email if user does not exist 
+      // ways (1. email unique, 2. upsert, 3. simple checking)
+
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query)
+      if(existingUser){
+        return res.send({message: 'User alredy exist',insertedId: null})
+      }
       const result = await userCollection.insertOne(user)
       res.send(result)
     })
